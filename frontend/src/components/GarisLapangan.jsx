d2c238d25eedfb6d51131e4210ba52cf89fc82e0
Yosub3 <y.narwastu11@gmail.com>
2025-05-28 14:45:07 +0700

    Home.jsx komponen
    Komponen dalam homepage dibagi bagi ke file components masing masing

---
+++ frontend/src/components/GarisLapangan.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function GarisLapangan() {
+    return (
+        <div className='w-full'>
+            <hr className="w-full my-12 h-0.5 border-t-0 bg-neutral-500" />
+        </div>
+    )
+}
+
+export default GarisLapangan
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
+import Lapangan from '../components/Lapangan'
+import GarisLapangan from '../components/GarisLapangan'
 
 function Home() {
   return (
@@ -207,30 +209,12 @@ function Home() {
             </a>
           </div>
         </div>
-        <div className='mt-10 grid grid-cols-3 gap-4 w-full px-3'>
-          <div className='w-full'>
-            <img
-              className="rounded-xl h-full object-cover object-center w-full mb-2"
-              src="https://cdn.pixabay.com/photo/2020/07/31/02/45/field-5451797_960_720.jpg"
-              alt="Gambar Lapangan" />
-          </div>
-          <div className='col-span-2 w-full my-5'>
-            <div className='flex h-full'>
-              <div className='shrink'>
-                <div class="flex flex-1 h-full">
-                  <div
-                    className="h-full w-0.5 bg-neutral-500 mr-2.5">
-                  </div>
-                </div>
-              </div>
-              <div className='grow'>
-                <h3 className='font-sans font-bold text-left text-xl'>
-                  Deskripsi
-                </h3>
-              </div>
-            </div>
-          </div>
-        </div>
+        <Lapangan/>
+        <GarisLapangan/>
+        <Lapangan/>
+        <GarisLapangan/>
+        <Lapangan/>
+        <GarisLapangan/>
       </div>
     </div>
   )
